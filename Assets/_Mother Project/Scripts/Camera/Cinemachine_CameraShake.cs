a93a16e9af54f9510dfefd365a5e3ddc43d96223
NUSHRAT <nushrat.j01@gmail.com>
2025-04-13 18:30:23 +0600

    implement event to call in animations to shake cam

--- Assets/_Mother Project/Scripts/Camera/Cinemachine_CameraShake.cs
+++ Assets/_Mother Project/Scripts/Camera/Cinemachine_CameraShake.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 
 public class Cinemachine_CameraShake : MonoBehaviour
 {
+    public CinemachineImpulseSource impulseSource;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
@@ -21,5 +24,38 @@ public class Cinemachine_CameraShake : MonoBehaviour
         {
             Debug.LogWarning("No CinemachineBrain found on the main camera. Did you forget to attach it, you chaotic artisan?");
         }
+
+        if(Input.GetKeyDown(KeyCode.V))
+        {
+            RumbleImpulse();
+        } 
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            ExplotionImpulse();
+        }
+    }
+
+    private void RumbleImpulse()
+    {
+        var definition = new CinemachineImpulseDefinition
+        {
+            m_ImpulseType = CinemachineImpulseDefinition.ImpulseTypes.Uniform,
+            m_ImpulseShape = CinemachineImpulseDefinition.ImpulseShapes.Rumble,
+        };
+
+        impulseSource.m_ImpulseDefinition = definition;
+        impulseSource.GenerateImpulseWithVelocity(Vector3.right);
+    }
+    private void ExplotionImpulse()
+    {
+        var definition = new CinemachineImpulseDefinition
+        {
+            //call event
+            m_ImpulseType = CinemachineImpulseDefinition.ImpulseTypes.Uniform,
+            m_ImpulseShape = CinemachineImpulseDefinition.ImpulseShapes.Explosion,
+        };
+
+        impulseSource.m_ImpulseDefinition = definition;
+        impulseSource.GenerateImpulse();
     }
 }
